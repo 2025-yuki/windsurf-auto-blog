d82b8f23e542751bc073b6de82343aac5dcdf513
2025-yuki <yuki.tajima1013@gmail.com>
2025-05-28 01:35:12 +0900

    ci: fix setup.py to include vendor windsurf

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="windsurf",
     version="0.1.0",
-    packages=find_packages(include=["windsurf", "windsurf.*"]),
+    # vendor 化した windsurf/ フォルダの中身をパッケージとして検索
+    packages=find_packages(where="windsurf", include=["windsurf", "windsurf.*"]),
+    # パッケージは windsurf/ の中にあることを指定
+    package_dir={"": "windsurf"},
     entry_points={
-        "console_scripts": ["windsurf=windsurf.console:windsurf"]
+        "console_scripts": [
+            "windsurf=windsurf.console:windsurf"
+        ]
     },
 )

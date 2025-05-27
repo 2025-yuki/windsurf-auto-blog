29c2c751e51c5941666a6cea191bdf045599c814
2025-yuki <yuki.tajima1013@gmail.com>
2025-05-28 01:50:39 +0900

    ci: simplify setup.py to find all packages

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="windsurf",
     version="0.1.0",
-    # vendor 化した windsurf/ フォルダの中身をパッケージとして検索
-    packages=find_packages(where="windsurf", include=["windsurf", "windsurf.*"]),
-    # パッケージは windsurf/ の中にあることを指定
-    package_dir={"": "windsurf"},
+    # リポジトリ直下にあるすべてのパッケージ(windsurf/)を探す
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "windsurf=windsurf.console:windsurf"
